7f438dd9a7a6df29d069ae63cd36b37759a029be
Alex <manesio2009@gmail.com>
2020-02-10 01:29:04 +0200

    add timer

--- js/main.js
+++ js/main.js
@@ -33,4 +33,56 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         };
     });
+
+
+    // timer
+
+    let deadLine = '2020-2-11';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor(t/(1000*3600));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000)
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = setZero(t.hours);
+            minutes.textContent = setZero(t.minutes);
+            seconds.textContent = setZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        };
+
+        function setZero(num) {
+            num = num + '';
+            if (num.length == 1) {
+                return '0' + num;
+            } else {
+                return num;
+            }
+        }
+    };
+
+    setClock('timer', deadLine);
 });
